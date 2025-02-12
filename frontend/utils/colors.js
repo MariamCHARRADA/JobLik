0144c290397466da5efcbddcd94611dff17f9c73
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-12 12:39:29 +0100

    UI colors

---
+++ frontend/utils/colors.js
@@ -0,0 +1,14 @@
+export default {
+    PRIMARY: '#17A2B8',
+    PLIGHT: "rgb(157, 208, 228)",
+    DARK: "#148A9C",
+    DARKER: "#117280",
+    WHITE: "#fff",
+    LIGHT_GRAY: "#f9f9f9",
+    PERFECT_GRAY: "rgb(242, 242, 243)",
+    LIGHTER_GRAY: "rgb(244, 247, 248)",
+    GREEN: "#rgb(9, 150, 91)",
+    LIGHT_GREEN: "rgba(50, 173, 85, 0.85)",
+    BLACK: "#000"
+    
+}
