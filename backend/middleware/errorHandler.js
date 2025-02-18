fa9fe10913942a9d731ed9178fcc8f17850d346a
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-18 01:22:13 +0100

    errorhandler

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,62 +1,44 @@
 const { constants } = require("../constants");
-
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-
   switch (statusCode) {
     case constants.VALIDATION_ERROR:
-      res.status(statusCode).json({
-        success: false,
-        title: "Validation Failed",
+      res.json({
+        title: "Validation failed",
         message: err.message,
-        stackTrace: process.env.NODE_ENV === "development" ? err.stack : null,
+        stackTrace: err.stack,
       });
       break;
-
     case constants.UNAUTHORIZED:
-      res.status(statusCode).json({
-        success: false,
-        title: "Unauthorized",
+      res.json({
+        title: "UNAUTHORIZED found",
         message: err.message,
-        stackTrace: process.env.NODE_ENV === "development" ? err.stack : null,
+        stackTrace: err.stack,
       });
       break;
-
     case constants.FORBIDDEN:
-      res.status(statusCode).json({
-        success: false,
+      res.json({
         title: "Forbidden",
         message: err.message,
-        stackTrace: process.env.NODE_ENV === "development" ? err.stack : null,
+        stackTrace: err.stack,
       });
       break;
-
     case constants.NOT_FOUND:
-      res.status(statusCode).json({
-        success: false,
-        title: "Not Found",
+      res.json({
+        title: "Not found",
         message: err.message,
-        stackTrace: process.env.NODE_ENV === "development" ? err.stack : null,
+        stackTrace: err.stack,
       });
       break;
-
     case constants.SERVER_ERROR:
-      res.status(statusCode).json({
-        success: false,
-        title: "Server Error",
+      res.json({
+        title: "Not server error",
         message: err.message,
-        stackTrace: process.env.NODE_ENV === "development" ? err.stack : null,
+        stackTrace: err.stack,
       });
       break;
-
     default:
-      console.error("Unhandled Error:", err);
-      res.status(500).json({
-        success: false,
-        title: "Internal Server Error",
-        message: "Something went wrong!",
-        stackTrace: process.env.NODE_ENV === "development" ? err.stack : null,
-      });
+      console.log("no error!");
       break;
   }
 };
